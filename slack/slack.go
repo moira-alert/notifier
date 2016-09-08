dcb54cb7571e513b489323243e1f6aeca4545a3d
vkolobaev <vkolobaev@avito.ru>
2016-09-08 14:15:29 +0000

    testing

--- slack/slack.go
+++ slack/slack.go
@@ -38,7 +38,7 @@ func (sender *Sender) SendEvents(events notifier.EventsData, contact notifier.Co
 	var message bytes.Buffer
 	state := events.GetSubjectState()
 	tags := trigger.GetTags()
-	message.WriteString(fmt.Sprintf("*%s* %s %s <%s/#/events/%s|%s>\n```", state, tags, sender.FrontURI, events[0].TriggerID, trigger.Desc, trigger.Name))
+	message.WriteString(fmt.Sprintf("*%s* <%s/#/events/%s|%s>\n```", state, tags, sender.FrontURI, events[0].TriggerID, trigger.Name))
 
 	icon := fmt.Sprintf("%s/public/fav72_ok.png", sender.FrontURI)
 	for _, event := range events {
